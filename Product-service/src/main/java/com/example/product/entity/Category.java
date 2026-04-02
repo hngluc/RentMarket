7c86f58fbdeea41ac6962419c471465a3a9dbb2f
HungThichCodee <danhcaolamhung9122004@gmail.com>
2026-04-02 20:15:00 +0700

    feat(product-service): add ItemStatus enum and Category entity with parent-child hierarchy

---
+++ Product-service/src/main/java/com/example/product/entity/Category.java
@@ -0,0 +1,39 @@
+package com.example.product.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = {"parent", "children"})
+@ToString(exclude = {"parent", "children"})
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> children;
+}
