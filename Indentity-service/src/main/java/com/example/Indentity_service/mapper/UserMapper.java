81a16217876c333520db92df42ef553d5f94309f
hngluc <nguyendanghoangluc@gmail.com>
2026-04-06 19:09:21 +0700

    Update UserMapper.java

--- Indentity-service/src/main/java/com/example/Indentity_service/mapper/UserMapper.java
+++ Indentity-service/src/main/java/com/example/Indentity_service/mapper/UserMapper.java
@@ -1,20 +1,20 @@
 package com.example.Indentity_service.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-
 import com.example.Indentity_service.dto.request.UserCreationRequest;
 import com.example.Indentity_service.dto.request.UserUpdateRequest;
 import com.example.Indentity_service.dto.response.UserResponse;
 import com.example.Indentity_service.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface UserMapper {
     User toUser(UserCreationRequest request);
 
     UserResponse toUserResponse(User user);
 
-    @Mapping(target = "roles", ignore = true)
+    @Mapping(target = "password", ignore = true)
     void updateUser(@MappingTarget User user, UserUpdateRequest request);
+
 }
